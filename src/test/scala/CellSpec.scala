89fcb5a9dded26c8854b384885871a3f0fd9ea04
Biju Rajan <brajan@Bijus-MacBook-Pro.local>
2017-07-26 16:38:06 -0400

    Using should matchers instead

--- src/test/scala/CellSpec.scala
+++ src/test/scala/CellSpec.scala
@@ -2,25 +2,25 @@ package com.biju
 
 import org.scalatest._
 
-class CellSpec extends FunSpec {
+class CellSpec extends FunSpec with Matchers {
 
   describe("alive cells") {
      it("should die with zero neighbors"){
        val cell = new Cell("ALIVE", 0)
 
-       assert(cell.nextGeneration() == "DEAD")
+       cell.nextGeneration() shouldEqual "DEAD"
      }
 
      it("should die with one neighbor"){
        val cell = new Cell("ALIVE", 1)
 
-       assert(cell.nextGeneration() == "DEAD")
+       cell.nextGeneration() shouldEqual "DEAD"
      }
 
     it("should not die with two neighbors"){
       val cell = new Cell("ALIVE", 2)
 
-      assert(cell.nextGeneration() == "ALIVE")
+      cell.nextGeneration() shouldEqual "ALIVE"
     }
   }
 }
